d2d48a012b8c21614dd8bfa3545c81ed883d976f
Pedroandradeproj <130566862+Pedroandradeproj@users.noreply.github.com>
2023-04-12 09:44:04 -0300

    Create Proj.py
    Atividade usando somente as ferramentas que foram ensinadas. Do site https://wiki.python.org.br/ListaDeExercicios

---
+++ Proj.py
@@ -0,0 +1,10 @@
+- 👋 Hello, I'm @Pedroandradeproj
+  - 👀 I am interested in
+  - 🌱 I am currently learning, I have three months of t.i text course.  "limited to use only what is taught", so as not to misuse it.
+  - 📫 How to reach me trabpedroandrade@gmail.com
+
+  <
+<!---
+Pedroandradeproj/Pedroandradeproj is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
