eefc0bb4795a3ca940978d22c2eb86842ba39eee
Pedro Andrade <130566862+Pedroandradeproj@users.noreply.github.com>
2023-04-12 10:50:01 -0300

    Delete test17.py

--- test17.py
+++
@@ -1,6 +0,0 @@
-# Programa que peça um número correspondente a um determinado ano 
-# e em seguida informe se este ano é ou não bissexto.
-print("Informe número correspondente a um determinado ano :")
-a=int(input())
-if (a%4==0 and a%100!=0) or (a%400==0): print("ano é bissexto")
-else :print("não e bissexto")
