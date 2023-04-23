df1ec3c6cdf845324a3d8fc2ddc4cc4afffca6c2
Pedro Andrade <130566862+Pedroandradeproj@users.noreply.github.com>
2023-04-23 19:16:18 -0300

    Add files via upload
    Lista de exercicio https://wiki.python.org.br/EstruturaSequencial.
    usando somente o que me foi ensinado

---
+++ test8.py
@@ -0,0 +1,9 @@
+#Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. 
+# Calcule e mostre o total do seu salário no referido mês.
+
+print("Quando você ganha por hora ?")
+h=int(input())
+print("Numero de horas trabalhadas ?")
+m=int(input())
+sm=h*m
+print("Total do seu salário no referido mês =",sm)
---
+++ test9.py
@@ -0,0 +1,5 @@
+# Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius
+print("Informe temperatura em grasus Fahrenheit ?")
+f=int(input())
+c=5*((f-32)/9)
+print("Tempera em graus celsius:",c)
